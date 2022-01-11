5ce497516df634ef96e729ffa73c22ad24bdbdef
Unlu <funlu@LAMU02DF31VMD6R.uhc.com>
2022-01-10 21:45:43 -0800

    checking in new paramters obtained for release

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -60,6 +60,9 @@ def go(args):
 
     logger.info(df.count())
 
+    #idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    #df = df[idx].copy()
+
     df.to_csv(args.output_artifact, index=False)
 
 
